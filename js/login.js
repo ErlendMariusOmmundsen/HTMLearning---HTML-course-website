2cadd52b4f29c46929bbd45dfce45a9e8b328ab0
Joakim Fremstad <joa.fre@gmail.com>
2018-10-24 13:38:43 +0200

    Add javascript

--- js/login.js
+++ js/login.js
@@ -1,1 +1,13 @@
-// Skeleton-file
+function showLogin() {
+  var loginbox = document.getElementById("loginbox");
+  var signupbox = document.getElementById("signupbox");
+  signupbox.style.display = "none";
+  loginbox.style.display = "block";
+}
+
+function showSignup() {
+  var loginbox = document.getElementById("loginbox");
+  var signupbox = document.getElementById("signupbox");
+  loginbox.style.display = "none";
+  signupbox.style.display = "block";
+}
