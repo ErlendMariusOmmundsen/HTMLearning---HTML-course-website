afac0d77c34dd6659f4a6ddfcf3f8feb05f79373
Joakim Fremstad <joa.fre@gmail.com>
2018-10-31 16:35:24 +0100

    Get questions from database

--- js/quiz.js
+++ js/quiz.js
@@ -1,6 +1,19 @@
 // Skeleton-file
+db = [["Question 1", "Alternative 1", "Alternative 2", "Alternative 3", "Alternative 4"]];
 
 function next() {
+  var j = 0;
+  var siteq = document.getElementById("question");
+  var sitea1 = document.getElementById("alt1text");
+  var sitea2 = document.getElementById("alt2text");
+  var sitea3 = document.getElementById("alt3text");
+  var sitea4 = document.getElementById("alt4text");
+  var quiz = [siteq, sitea1, sitea2, sitea3, sitea4]
+
+  for (let i = 0; i < quiz.length; ++i) {
+    console.log(db[j][i]);
+    quiz[i].innerHTML = db[j][i];
+  }
 
 }
 
