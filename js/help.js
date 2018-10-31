b7d31fdee7963a9fa071f554db90cc2108e1e51f
Joakim Fremstad <jfremstad@users.noreply.github.com>
2018-10-31 12:08:06 +0000

    FAQ (#7)
    * Add simple answerToggle and hover css
    
    * Use h2 for questions
    
    * Add more fill for FAQ. TODO: add some better questions
    
    * Add fonts
    
    * Move to help.css

--- js/help.js
+++ js/help.js
@@ -1,1 +1,10 @@
-// Skeleton-file
+function toggleAnswer(question) {
+  var answer = question.parentElement.getElementsByClassName("faqanswer")[0];
+  var isopen = answer.classList.contains("open");
+
+  if (isopen) {
+    answer.classList.remove("open");
+  } else {
+    answer.classList.add("open");
+  }
+}
