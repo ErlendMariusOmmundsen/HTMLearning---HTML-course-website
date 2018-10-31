223e4b1abe8203dfd66eec90801ed31051120792
Joakim Fremstad <joa.fre@gmail.com>
2018-10-31 15:40:37 +0100

    Add navbar and footer to frontpage

--- js/includeHTML.js
+++ js/includeHTML.js
@@ -1,18 +1,18 @@
 function setNavbar() {
   navbar = document.getElementById("placeholder-navbar");
-  navbar.id = "nav_bar"
+  navbar.id = "nav_bar";
   navbar.innerHTML = `
       <a href="frontpage.html"><img id="logo_element" src="img/logo.png" alt="logo"></a>
       <button onclick="location.href='frontpage.html';" id="home_input" type="button" >Home</button>
       <button onclick="location.href='courses_overview.html';" id="courses_input" type="button" >Courses</button>
       <button onclick="location.href='help.html';" id="faq_input" type="button" >FAQ/Help</button>
       <button onclick="location.href='about.html';" id="About_input" type="button" >About us</button>
-      <button onclick="location.href='login.html';" id="Login_input" type="button" >Login</button>`
+      <button onclick="location.href='login.html';" id="Login_input" type="button" >Login</button>`;
 };
 
 function setFooter() {
   footer = document.getElementById("placeholder-footer");
-  footer.id = "footer"
+  footer.id = "footer";
   footer.innerHTML = `
 <footer>
 <div id="info">
@@ -42,7 +42,7 @@ function setFooter() {
 </div>
 <p style="color:grey;font-size: 10px;">Created by IT2805 Webteknologi group 1 </p>
 </footer>
-`
+`;
 };
 
 function setAll() {
