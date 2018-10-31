9dacb83582d0bbbb46f2e60110ff5b37adc9e05b
laerke-s <laerkes@stud.ntnu.no>
2018-10-31 15:32:06 +0100

    Added navbar to courses and courses overview
    Changed the includeHTML to work for html files inside another folder.
    (the courses files)

--- js/includeHTML.js
+++ js/includeHTML.js
@@ -1,11 +1,16 @@
 function setNavbar() {
-  navbar = document.getElementById("placeholder-navbar");
-  navbar.id = "nav_bar"
-  navbar.innerHTML = `
-      <a href="frontpage.html"><img id="logo_element" src="img/logo.png" alt="logo"></a>
-      <button onclick="location.href='frontpage.html';" id="home_input" type="button" >Home</button>
-      <button onclick="location.href='courses_overview.html';" id="courses_input" type="button" >Courses</button>
-      <button onclick="location.href='help.html';" id="faq_input" type="button" >FAQ/Help</button>
-      <button onclick="location.href='about.html';" id="About_input" type="button" >About us</button>
-      <button onclick="location.href='login.html';" id="Login_input" type="button" >Login</button>`
+    navbar = document.getElementById("placeholder-navbar");
+    navbar.id = "nav_bar";
+    let folder_up = '';
+    if (typeof inside_folder !== 'undefined') {
+        folder_up = '../';
+    };
+    let navbar_html = `
+      <a href="frontpage.html"><img id="logo_element" src="` + folder_up + `img/logo.png" alt="logo"></a>
+      <button onclick="location.href='` + folder_up + `frontpage.html';" id="home_input" type="button" >Home</button>
+      <button onclick="location.href='` + folder_up + `courses_overview.html';" id="courses_input" type="button" >Courses</button>
+      <button onclick="location.href='` + folder_up + `help.html';" id="faq_input" type="button" >FAQ/Help</button>
+      <button onclick="location.href='` + folder_up + `about.html';" id="About_input" type="button" >About us</button>
+      <button onclick="location.href='` + folder_up + `login.html';" id="Login_input" type="button" >Login</button>`;
+    navbar.innerHTML = navbar_html;
 };
