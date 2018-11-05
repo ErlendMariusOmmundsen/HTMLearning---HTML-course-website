02547fe8afbfb1c212ad918ff8fd7503096a2f0a
Unknown <martin.skurtveit@gmail.com>
2018-11-05 11:50:57 +0100

    Add lecturer

--- js/includeHTML.js
+++ js/includeHTML.js
@@ -29,20 +29,21 @@ function setFooter() {
 		Lorem ipsum dolor sit amet, consectateur adispcing elit. Fusce euismod 
 		conballis velit, eu auctor lacus vehicula sit amet.
 	</p>
-
+</div>
+<div id="links">
 	<div class="grid-container">
 	<div class="item1"><img class="icon" src="` + folder_up + `img/Mailicon.png" alt="E-mail:"/></div>
 	<div class="item2"><a href="mailto:help@htmlearning.com?Subject=HTMLearning" target="_top">help@htmlearning.com</a></div>
 	<div class="item3"><img class="icon" src="` + folder_up + `img/phoneicon.png" alt="Phone:"/></div>
 	<div class="item4"><a href="tel:+4700000000">+47 000 00 000 </a></div>
+	<div class="item5"><img class="icon" src="` + folder_up + `img/positionicon.png" alt="Pos:"/></div>
+	<div class="item6"><a href="https://www.google.com/maps/place/Sondre+gate+4,+7011+Trondheim">
+		Søndre gate 4, Trondheim</a></div>
 	</div>
 </div>
 <div id="map">
 	<div id="googlemaps" style="width:320px;height:200px;" >Map to location</div>
-	<div class="grid-container2">
-	<div class="item5"><img class="icon" src="` + folder_up + `img/positionicon.png" alt="Pos:"/></div>
-	<div class="item6"><a href="https://www.google.com/maps/place/Sondre+gate+4,+7011+Trondheim">
-		Søndre gate 4, Trondheim</a></div>
+	
 	</div>
 
 <script src="js/maps.js"></script>
