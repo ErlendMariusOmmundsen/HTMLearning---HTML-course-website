9215608c92b953045b24ad2e7b4805d21db6ec78
Joakim Fremstad <jfremstad@users.noreply.github.com>
2018-11-05 11:03:27 +0000

    Fix navbar in about. Set home to content (#11)

--- js/includeHTML.js
+++ js/includeHTML.js
@@ -7,7 +7,7 @@ function setNavbar() {
     };
     let navbar_html = `
       <a href="` + folder_up + `frontpage.html"><img id="logo_element" src="` + folder_up + `img/logo.png" alt="logo"></a>
-      <button onclick="location.href='` + folder_up + `frontpage.html';" id="home_input" type="button" >Home</button>
+      <button onclick="location.href='` + folder_up + `frontpage.html#nav_bar';" id="home_input" type="button" >Home</button>
       <button onclick="location.href='` + folder_up + `courses_overview.html';" id="courses_input" type="button" >Courses</button>
       <button onclick="location.href='` + folder_up + `help.html';" id="faq_input" type="button" >FAQ/Help</button>
       <button onclick="location.href='` + folder_up + `about.html';" id="About_input" type="button" >About us</button>
