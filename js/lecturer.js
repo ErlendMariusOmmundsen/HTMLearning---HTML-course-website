65decdbdf02a1ab9a5c51dc664893bec34b87786
Unknown <martin.skurtveit@gmail.com>
2018-11-08 11:27:31 +0100

    Some hotfixes to complete design

--- js/lecturer.js
+++ js/lecturer.js
@@ -11,8 +11,8 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
+  var slides = document.getElementsByClassName("lecturerSlides");
+  var dots = document.getElementsByClassName("lecturerRef");
   var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
